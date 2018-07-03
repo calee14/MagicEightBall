d63bfdd2b0895ba425044b2c9980b34351516c98
calee14 <caplee1000@gmail.com>
2018-07-03 09:49:57 -0700

    added shake gesture

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
     
@@ -23,12 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func generateAnswer() {
+        let maxIndex = UInt32(answers.count)
+        
+        let randIndex = Int(arc4random_uniform(maxIndex))
+        
+        answerLabel.text = answers[randIndex]
+    }
     @IBAction func shakeButtonTapped(_ sender: Any) {
         print("shake it like a mad man")
-        
-        answerLabel.text = "button was tapped"
+        generateAnswer()
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        
+        generateAnswer()
+    }
 
 
 }
