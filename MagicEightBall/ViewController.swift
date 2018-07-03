a1fc2de174e2c14a3980b0c7ad14c1d33808397f
calee14 <caplee1000@gmail.com>
2018-07-03 10:01:59 -0700

    added random startup idea ball

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     
     let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
     
+    let projects = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"]
+    
+    let targets = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
+
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
     
+    @IBOutlet weak var projectLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateStartUp()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -34,15 +42,24 @@ class ViewController: UIViewController {
         
         answerLabel.text = answers[randIndex]
     }
+    func generateStartUp() {
+        let maxIndex_p = UInt32(projects.count)
+        let maxIndex_t = UInt32(targets.count)
+        
+        let randIndex_p = Int(arc4random_uniform(maxIndex_p))
+        let randIndex_t = Int(arc4random_uniform(maxIndex_t))
+        
+        projectLabel.text = projects[randIndex_p]
+        targetLabel.text = targets[randIndex_t]
+    }
     @IBAction func shakeButtonTapped(_ sender: Any) {
         print("shake it like a mad man")
-        generateAnswer()
+        generateStartUp()
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         guard motion == .motionShake else { return }
-        
-        generateAnswer()
+        generateStartUp()
     }
 
 
